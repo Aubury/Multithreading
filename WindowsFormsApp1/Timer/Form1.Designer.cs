2ca9fc11ea27cb1a2cea2c04a54e2bbfa5da8973
Aubury <aubury@ukr.net>
2018-05-27 17:10:03 +0300

    Change_name+Thread_Timer

---
+++ WindowsFormsApp1/Timer/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Start.Location = new System.Drawing.Point(16, 30);
+            this.Start.Margin = new System.Windows.Forms.Padding(4);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(172, 65);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(385, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(415, 264);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(16, 174);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(172, 74);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 316);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Start);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ WindowsFormsApp1/Timer/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+      public int i { set; get; }
+        public System.Threading.Timer Timer;
+     
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        public void DoWork()
+        {
+            
+            for( int  j=0;  j<101; j++)
+            {
+           
+                i = j;
+                Thread.Sleep(500);
+                this.Invoke((Action)delegate
+                {
+                    this.Text = string.Format("Processing {0}  % Completed ", i.ToString());
+                });
+
+                if (i == 100 ) { return; }
+               
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+        Task.Factory.StartNew(() =>
+            {
+                DoWork();
+            });
+           
+           Timer =  new System.Threading.Timer(_ListBox,100,0,10000);
+            if(i==100)
+            {
+                Timer.Change(Timeout.Infinite, Timeout.Infinite);
+            }
+           
+        }
+        private void _ListBox(object sender)
+        {
+            ListBox.CheckForIllegalCrossThreadCalls = false;
+          if(i != 0 && i<101)
+            {
+                 listBox1.Items.Add(i.ToString() + " % Completed   ");
+            }
+         if(i==100)
+            {
+                // listBox1.Items.Add("Progress completed!");
+                Timer.Change(Timeout.Infinite, Timeout.Infinite);
+            }
+
+             
+        }
+        private void Stop_Click(object sender, EventArgs e)
+        {
+               this.Close();
+        }
+    }
+}
